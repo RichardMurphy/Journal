856b13c33c7e1cbdbf41e66bd3401aa8cdf12ad8
RichardMurphy <rickmurphy@outlook.com>
2016-03-11 15:32:18 +1100

    Added monthly db

--- Journal/Form1.cs
+++ Journal/Form1.cs
@@ -15,45 +15,19 @@ namespace MonthlyCalander
     {
         string selectedDate;
         SQLiteConnection my_dbConnection;        
-        bool recordExists;
+        bool recordExists, isMonthly = false;
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        //private void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
-        //{
-        //    labelDate.Text = monthCalendar1.SelectionStart.ToString("ddd dd-M-yyyy");
-        //    selectedDate = monthCalendar1.SelectionStart.ToString("yyyy-MM-dd");           
-
-        //    my_dbConnection = new SQLiteConnection("Data Source=myDiary.db;Version=3;");
-        //    my_dbConnection.Open();
-
-        //    string search = "SELECT DATE FROM diary WHERE DATE = '" + selectedDate + "';";
-        //    SQLiteCommand command = new SQLiteCommand(search, my_dbConnection);
-
-        //    SQLiteDataReader reader = command.ExecuteReader();
-
-        //    MessageBox.Show(reader["DATE"].ToString()); 
-            
-        //    //if (selectedDate == reader["DATE"].ToString())         
-
-        //   // richTextBox1.Text = reader["NOTE"].ToString();
-
-            
-
-        //    my_dbConnection.Close();
-        //}
-
-       
-
         private void Form1_Load(object sender, EventArgs e)
         {                        
             //get note for selected date
             labelDate.Text = monthCalendar1.SelectionStart.ToString("ddd dd-M-yyyy");
             selectedDate = monthCalendar1.SelectionStart.ToString("yyyy-MM-dd");
-            
+                                  
             my_dbConnection = new SQLiteConnection("Data Source=myDiary.db;Version=3;");
             my_dbConnection.Open();
 
@@ -76,12 +50,27 @@ namespace MonthlyCalander
         private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
         {
             labelDate.Text = monthCalendar1.SelectionEnd.ToString("ddd dd-M-yyyy");            
-            selectedDate = monthCalendar1.SelectionEnd.ToString("yyyy-MM-dd");            
+            selectedDate = monthCalendar1.SelectionEnd.ToString("yyyy-MM-dd");
 
             my_dbConnection = new SQLiteConnection("Data Source=myDiary.db;Version=3;");
-            my_dbConnection.Open();            
+            my_dbConnection.Open();
 
-            string search = "SELECT NOTE FROM diary WHERE DATE = '" + selectedDate + "';";
+            string search;
+
+            TimeSpan range = monthCalendar1.SelectionEnd - monthCalendar1.SelectionStart; //add monthly record            
+            
+            if (range.ToString() == "6.00:00:00")
+            {               
+                search = "SELECT NOTE FROM monthly WHERE DATE = '" + selectedDate + "';";
+                labelDate.Text = monthCalendar1.SelectionEnd.ToString("MMMM");
+                isMonthly = true;
+            }
+            else
+            {
+                search = "SELECT NOTE FROM diary WHERE DATE = '" + selectedDate + "';";
+                isMonthly = false;
+            }               
+            
             SQLiteCommand command = new SQLiteCommand(search, my_dbConnection);
 
             SQLiteDataReader reader = command.ExecuteReader();
@@ -94,23 +83,46 @@ namespace MonthlyCalander
         private void buttonSaveRecord_Click(object sender, EventArgs e)
         {
             selectedDate = monthCalendar1.SelectionEnd.ToString("yyyy-MM-dd");
-           // MessageBox.Show(selectedDate);
-
+           
             my_dbConnection = new SQLiteConnection("Data Source=myDiary.db;Version=3;");
             my_dbConnection.Open();
 
-            string search = "SELECT DATE FROM diary WHERE DATE = '" + selectedDate + "';";
+            string search;
+            if (isMonthly)
+            {
+                search = "SELECT DATE FROM monthly WHERE DATE = '" + selectedDate + "';";
+            }
+            else
+            {
+                search = "SELECT DATE FROM diary WHERE DATE = '" + selectedDate + "';";
+            }
+
+           
             SQLiteCommand command = new SQLiteCommand(search, my_dbConnection);
             SQLiteDataReader reader = command.ExecuteReader();
 
             string sql;
-            if (reader["DATE"].ToString() == selectedDate)
+            if (isMonthly)
             {
-                sql = "UPDATE diary SET NOTE = '" + textBoxNotes.Text + "' WHERE DATE = '" + selectedDate + "';";
+                if (reader["DATE"].ToString() == selectedDate)
+                {
+                    sql = "UPDATE monthly SET NOTE = '" + textBoxNotes.Text + "' WHERE DATE = '" + selectedDate + "';";
+                }
+                else
+                {
+                    sql = "INSERT INTO monthly VALUES ('" + selectedDate + "','" + textBoxNotes.Text + "');";
+                }
             }
             else
             {
-                sql = "INSERT INTO diary VALUES ('" + selectedDate + "','" + textBoxNotes.Text + "');";
+                if (reader["DATE"].ToString() == selectedDate)
+                {
+                    sql = "UPDATE diary SET NOTE = '" + textBoxNotes.Text + "' WHERE DATE = '" + selectedDate + "';";
+                }
+                else
+                {
+                    sql = "INSERT INTO diary VALUES ('" + selectedDate + "','" + textBoxNotes.Text + "');";
+                }
             }
 
             command = new SQLiteCommand(sql, my_dbConnection);
